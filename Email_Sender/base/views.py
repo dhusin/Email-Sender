662fb5b202da1e0b9e0b52cd27beb91f4bf0e008
dhusin <dhusin01@gmail.com>
2022-08-23 23:19:18 +0530

    added project

---
+++ Email_Sender/base/views.py
@@ -0,0 +1,7 @@
+import http
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request,'home.html')
